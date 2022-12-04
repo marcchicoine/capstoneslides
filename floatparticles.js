d217c5105cae4b6c4446b5371eda561cbcd13bdb
Marc Chicoine <marcchicoine2017@gmail.com>
2022-12-03 22:32:45 -0500

    d

--- floatparticles.js
+++ floatparticles.js
@@ -9,7 +9,7 @@ for (let i = 0; i < numBalls; i++) {
   ball.classList.add('ball')
   ball.style.background = colors[Math.floor(Math.random() * colors.length)]
   ball.style.left = `${Math.floor(Math.random() * 100)}vw`
-  ball.style.top = `${Math.floor(Math.random() * 1000)}vh`
+  ball.style.top = `${Math.floor(Math.random() * 1400)}vh`
   ball.style.transform = `scale(${Math.random()})`
   ball.style.width = `${Math.random()}em`
   ball.style.height = ball.style.width
